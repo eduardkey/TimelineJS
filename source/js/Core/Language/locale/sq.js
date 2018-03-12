029c29dd20b553c04deabfdd701eca77e44ad096
eduardkey <37300904+eduardkey@users.noreply.github.com>
2018-03-12 15:59:16 +0100

    Albanian language sq.js
    Albanian translation.

---
+++ source/js/Core/Language/locale/sq.js
@@ -0,0 +1,38 @@
+/* English LANGUAGE 
+================================================== */
+if(typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "sq",
+		api: {
+			wikipedia: "sq"
+		},
+		date: {
+			month: ["Janar", "Shkurt", "Mars", "Prill", "Maj", "Qeshor", "Korrik", "Gusht", "Shtator", "Tetor", "Nëntor", "Dhjetor"],
+			month_abbr: ["Jan.", "Shk.", "Mars", "Prill", "Maj", "Qesh", "Korr", "Gusht.", "Sht.", "Tet.", "Nën.", "Dhj."],
+			day: ["Djel","Hënë", "Martë", "Mërkurë", "Enjte", "Premte", "Shtunë"],
+			day_abbr: ["Dj.","Hën.", "Mar.", "Mër.", "Enj.", "Pre.", "Sht."]
+		}, 
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm yyyy",
+			full_short: "mmm d",
+			full: "mmmm d',' yyyy",
+			time_short: "h:MM:ss TT",
+			time_no_seconds_short: "h:MM TT",
+			time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+			full_long: "mmm d',' yyyy 'at' h:MM TT",
+			full_long_small_date: "h:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Duke ngarkuar Timeline... ",
+			return_to_title: "Kthehu tek Titulli",
+			expand_timeline: "Zmadho Timeline",
+			contract_timeline: "Zvogëlo Timeline",
+			wikipedia: "Nga Wikipedia, enciklopedia falas",
+			loading_content: "Duke ngarkuar të dhënat",
+			loading: "Duke Ngarkuar",
+			swipe_nav: "Swipe për te Lundruar"
+		}
+	}
+}
